ac51b889222f35bb3b99ec8cabc63022b25c7d96
ehammo <ehammo@cin.ufpe.br>
2016-07-10 09:32:24 -0300

    add localFrag v1

---
+++ app/src/main/java/com/codecup/childsparty/LocalFragment.java
@@ -0,0 +1,95 @@
+package com.codecup.childsparty;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class LocalFragment extends Fragment{
+    String[] web1000 = {
+            "Bolo comum",
+            "Bolas de encher",
+            "Tema para decoração, lembrancinhas e convites",
+            "Salgados e doces"
+    };
+    String[] web3000 = {
+            "Bolo decorativo",
+            "Tema para decoração, lembrancinhas e convites personalizadas",
+            "Salgados e doces",
+            "Recreador(Magico, palhaço)"
+    };
+    String[] web5000 = {
+            "Bolo decorativo",
+            "Buffet com garçom",
+            "Tema para decoração, lembrancinhas e convites personalizada",
+            "Salgados e doces",
+            "Recreador",
+            "Serviços alimenticeos",
+            "Banda"
+
+
+    };
+    String[] web10000 = {
+            "Bolo decorativo",
+            "Espaço lazer",
+            "Buffet com garçom",
+            "Tema para decoração, lembrancinhas e convites personalizada",
+            "Salgados e doces",
+            "Recreador",
+            "Serviços alimenticeos",
+            "Banda",
+            "Brinquedos diversos"
+    };
+
+    Integer[] fotos1000 = {};
+    Integer[] fotos3000 = {};
+    Integer[] fotos5000 = {};
+    Integer[] fotos10000 = {};
+
+    ListView list;
+    FornecedorAdapter adapter;
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this FornecedoresFragment
+        View view =  inflater.inflate(R.layout.local,
+                container, false);
+        getActivity().setTitle("Organize sua festa: Local");
+        Float orcamento = (Float)getArguments().get("orcamento");
+        list = (ListView) view.findViewById(R.id.listView);
+
+        //data para o Adapter diferente para cada um dos orçamentos
+        if (orcamento<1000){
+            adapter = new FornecedorAdapter(getActivity(), web1000, fotos1000);
+        }else if(orcamento>=1000&&orcamento<3000){
+            adapter = new FornecedorAdapter(getActivity(), web3000, fotos3000);
+        }else if(orcamento>=3000&&orcamento<5000){
+            adapter = new FornecedorAdapter(getActivity(), web5000, fotos5000);
+        }else{
+            adapter = new FornecedorAdapter(getActivity(), web10000, fotos10000);
+        }
+        list.setAdapter(adapter);
+        Button b = (Button)view.findViewById(R.id.bt_prox);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                //errado
+                FornecedorFragment f =  new FornecedorFragment();
+                ft.replace(R.id.fragment,f);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        return view;
+    }
+
+    public LocalFragment(){}
+
+}
--- app/src/main/java/com/codecup/childsparty/OrganizarFragment.java
+++ app/src/main/java/com/codecup/childsparty/OrganizarFragment.java
@@ -2,14 +2,21 @@ package com.codecup.childsparty;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class OrganizarFragment extends Fragment{
+    Float orcamento;
+    View view2;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -17,7 +24,32 @@ public class OrganizarFragment extends Fragment{
         // Inflate the layout for this FornecedoresFragment
         View view =  inflater.inflate(R.layout.organize,
                 container, false);
+        view2=view;
         getActivity().setTitle("Organize sua festa");
+
+        Button b = (Button)view.findViewById(R.id.bt_prox);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                LocalFragment f =  new LocalFragment();
+                Bundle bundle = new Bundle();
+
+                if(((EditText)view2.findViewById(R.id.orc)).getText().toString() != "") {
+                    Log.e("orc",((EditText) view2.findViewById(R.id.orc)).getText().toString());
+                    orcamento = Float.valueOf(((EditText) view2.findViewById(R.id.orc)).getText().toString());
+                    bundle.putFloat("orcamento",orcamento);
+                    f.setArguments(bundle);
+                    ft.replace(R.id.fragment,f);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                }else{
+                    Toast.makeText(getActivity(),"erro",Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
+
         return view;
     }
 
