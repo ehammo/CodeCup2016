7c3579baa17d849185d90be52dc9e30b4725e2ef
Pedro Sereno <psg2@cin.ufpe.br>
2016-07-10 16:18:03 -0300

    Tutorial screens

--- app/src/main/java/com/codecup/childsparty/Tutorial2.java
+++ app/src/main/java/com/codecup/childsparty/Tutorial2.java
@@ -1,13 +1,13 @@
 package com.codecup.childsparty;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class Tutorial2 extends AppCompatActivity {
 
-    private final long SPLASH_DISPLAY_LENGTH = 200;
+    private final long SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                startActivity(new Intent(SplashActivity.this, Tutorial1.class));
+                startActivity(new Intent(Tutorial2.this, Tutorial3.class));
                 finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
