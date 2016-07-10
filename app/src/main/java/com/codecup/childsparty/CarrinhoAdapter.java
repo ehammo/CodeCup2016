f03679fe1dca65d94ab7f91c430bd19586d40e42
gaabs <gaabs@cin.ufpe.br>
2016-07-10 14:41:40 -0300

    layout da tela de carrinho e parte necessárias

---
+++ app/src/main/java/com/codecup/childsparty/CarrinhoAdapter.java
@@ -0,0 +1,68 @@
+package com.codecup.childsparty;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gio on 7/10/2016.
+ */
+public class CarrinhoAdapter extends BaseAdapter{
+    private Activity context;
+    private ArrayList<String> names;
+    private ArrayList<Integer> imgs;
+
+    public CarrinhoAdapter(Activity context, ArrayList<String> names, ArrayList<Integer> imgs) {
+        this.context = context;
+        this.names = names;
+        this.imgs = imgs;
+    }
+
+    @Override
+    public int getCount() {
+        return names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View rowView= inflater.inflate(R.layout.item_carrinho, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.carrinho_item_name);
+        TextView preco = (TextView) rowView.findViewById(R.id.carrinho_item_preco);
+
+        Button deleteBtn = (Button)rowView.findViewById(R.id.delete_btn);
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                names.remove(position); //or some other task
+                imgs.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtTitle.setText(names.get(position));
+        preco.setText(imgs.get(position));
+
+        return rowView;
+    }
+}
